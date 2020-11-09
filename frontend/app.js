afd0cf60a61734d4117aa8d39a6b2a9f3b9fc78c
SofieTorch <sofie.torch@outlook.com>
2020-11-09 18:28:45 -0400

    vistas_terminadas

--- frontend/app.js
+++ frontend/app.js
@@ -10,4 +10,19 @@ app.config(function($routeProvider) {
             templateUrl: './pages/cafeteria/home/cafeteria-home.html',
             controller: 'cafeteriaHomeController'
         })
+        .when('/iniciar-sesion', {
+            templateUrl: './pages/cafeteria/log-in/log-in.html'
+        })
+        .when('/registro', {
+            templateUrl: './pages/cafeteria/sign-up/sign-up.html'
+        })
+        .when('/cafeteria/reservas', {
+            templateUrl: './pages/cafeteria/reservas/reservas.html'
+        })
+        .when('/cafeteria/inicio-sesion', {
+            templateUrl: './pages/cafeteria/log-in02/inicioSesion.html'
+        })
+        .when('/cafeteria/pendientes', {
+            templateUrl: './pages/cafeteria/pendientes/pendientes.html'
+        })
 })
